7528a48fccb2ba85e488074391f19b2b45d5c10c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-02 11:28:02 +0200

    beautify printing

--- py_test.py
+++ py_test.py
@@ -41,7 +41,7 @@ def py_test():
     }
     total_points = 0
     total_max = 0
-    args = ['-k', '','--disable-warnings', ''] #--disable-warnings is used to suppress warnings from py_test.py
+    args = ['-k', '','--disable-warnings', '-q'] #--disable-warnings is used to suppress warnings from py_test.py
     for casenum, case in enumerate(cases_list):
         result = {
             'name': case.name,
