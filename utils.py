9766a4b398327a7d3b1179a5329f316ac956e5a8
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-03 10:58:06 +0200

    put Colors in Util Class

---
+++ utils.py
@@ -0,0 +1,15 @@
+"""
+This file contains utility functions and classes that are used in the main script.
+"""
+
+class bcolors:
+    """Class to define colors for console output"""
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
